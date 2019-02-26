441fbc793371c922f9d58d7d2548a9e3c7a05276
Guest User <Guest@epicodus-19.local>
2019-02-26 13:04:12 -0800

    finish todolist

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -14,7 +14,7 @@ namespace ToDoList.Tests
       Item.ClearAll();
     }
 
-    public ItemTests()
+    public ItemTest()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=to_do_list_test;";
     }
@@ -57,7 +57,7 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
+    public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
     {
       //Arrange
       List<Item> newList = new List<Item> { };
@@ -70,13 +70,41 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
+    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+    {
+      // Arrange, Act
+      Item firstItem = new Item("Mow the lawn");
+      Item secondItem = new Item("Mow the lawn");
+
+      // Assert
+      Assert.AreEqual(firstItem, secondItem);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //Arrange
+      Item testItem = new Item("Mow the lawn");
+
+      //Act
+      testItem.Save();
+      List<Item> result = Item.GetAll();
+      List<Item> testList = new List<Item>{testItem};
+
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+    }
+
+    [TestMethod]
     public void GetAll_ReturnsItems_ItemList()
     {
       //Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
+      newItem1.Save();
       Item newItem2 = new Item(description02);
+      newItem2.Save();
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
       //Act
@@ -87,33 +115,48 @@ namespace ToDoList.Tests
     }
 
     [TestMethod]
-    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    public void Save_AssignsIdToObject_Id()
     {
       //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
+      Item testItem = new Item("Mow the lawn");
 
       //Act
-      int result = newItem.GetId();
+      testItem.Save();
+      Item savedItem = Item.GetAll()[0];
+
+      int result = savedItem.GetId();
+      int testId = testItem.GetId();
 
       //Assert
-      Assert.AreEqual(1, result);
+      Assert.AreEqual(testId, result);
     }
-
+    //
+    // [TestMethod]
+    // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    // {
+    //   //Arrange
+    //   string description = "Walk the dog.";
+    //   Item newItem = new Item(description);
+    //
+    //   //Act
+    //   int result = newItem.GetId();
+    //
+    //   //Assert
+    //   Assert.AreEqual(1, result);
+    // }
+    //
     [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
     {
       //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+      Item testItem = new Item("Mow the lawn");
+      testItem.Save();
 
       //Act
-      Item result = Item.Find(2);
+      Item foundItem = Item.Find(testItem.GetId());
 
       //Assert
-      Assert.AreEqual(newItem2, result);
+      Assert.AreEqual(testItem, foundItem);
     }
 
   }
